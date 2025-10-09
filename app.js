9952b6b2cc3fd4e327b9b30d8d0378138a428daf
Fernanda0812 <costa.souza.fernanda@escola.pr.gov.br>
2025-10-09 09:07:44 -0300

    comecando

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-let titulo= document.
+let titulo= document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+let campo = document.querySelector(tag)
+campo.innerHTML = texto
+
+function verificarChute() {
+    console.log('O botão foi clicado!')
+}

d8ef428fe3b1d438fec39edb84da7a2abcf86ae7
Fernanda0812 <costa.souza.fernanda@escola.pr.gov.br>
2025-10-23 09:09:01 -0300

    ok

--- app.js
+++ app.js
@@ -13,21 +13,33 @@ function verificarChute() {
    let chute = document.querySelector('input').value;
    
    if (chute == numeroSecreto) {
-       exibirTextoNaTela ('h1', 'Acertou!');
-       let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-       let mensagemTentativas = "Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!;
-       exibirTextoNaTela ('p', mensagemTentativas);
-     } else {
-       if (chute > numeroSecreto) {
-       exibirTextoNaTela('p', 'O número secreto é menor');
-     } else {
-      exibirTextoNaTela('p', 'O número secreto é maior');
-                        }
-        }
-}
+  exibirTextoNaTela ('h1', 'Acertou!');
+   let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+   let mensagemTentativas = "Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!;
+  exibirTextoNaTela ('p', mensagemTentativas);
+  document.getElementById('reiniciar').removeAttribute('disabled');
+   } else {
+   if (chute > numeroSecreto) {
+   exibirTextoNaTela('p', 'O número secreto é menor');
+   } else {
+  exibirTextoNaTela('p', 'O número secreto é maior');
+  }
+   tentativas++;    
+   limparCampo();
+
+
    
         
     
 
 
-   
+   function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+   }
+
+   function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+   }

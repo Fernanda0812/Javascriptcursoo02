d068d96696363d662ebd1528b2f369b02fd9db1c
Fernanda0812 <costa.souza.fernanda@escola.pr.gov.br>
2025-10-24 11:50:57 -0300

    ok

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
-let numeroSecreto = gerarNumeroALeatorio();
+let listaDeNumeroSorteados = [];
+let numeroLimite = 10;let 
+numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
-   let campo = document.querySelector(tag)
+let campo = document.querySelector(tag);
 campo.innerHTML = texto;
+responsiveVoice.speak(texto, 'brazilian Portuguese Female', {rate:1.2});
 }
+    
+function exibirMensagemInicial() {
+   exibirTextoNaTela('h1', 'Jogo do número secreto');
+   exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+} 
 
-exibirTextoNaTela('h1', 'Jogo do número secreto')
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+exibirMensagemNaTela();
 
 function verificarChute() {
    let chute = document.querySelector('input').value;
-   
+
    if (chute == numeroSecreto) {
-  exibirTextoNaTela ('h1', 'Acertou!');
-   let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-   let mensagemTentativas = "Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!;
-  exibirTextoNaTela ('p', mensagemTentativas);
-  document.getElementById('reiniciar').removeAttribute('disabled');
-   } else {
-   if (chute > numeroSecreto) {
-   exibirTextoNaTela('p', 'O número secreto é menor');
-   } else {
-  exibirTextoNaTela('p', 'O número secreto é maior');
-  }
-   tentativas++;    
-   limparCampo();
+      exibirTextoNaTela('h1', 'Acertou!');
+      let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+      let mensagemTentativas = 'Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!';
+      exibirTextoNaTela
+
+   }else {
 
 
-   
-        
-    
 
 
-   function limparCampo() {
-    chute = document.querySelector('input');
-    chute.value = '';
+
+
+
    }
-
-   function reiniciarJogo() {
-    numeroSecreto = gerarNumeroAleatorio();
-    limparCampo();
-    tentativas = 1;
-   }
+} 
